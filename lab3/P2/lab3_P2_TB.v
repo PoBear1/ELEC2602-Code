5c3002f7a42258c37256d6f29c8967d4607db435
po.bear169 <po.bear169@gmail.com>
2026-03-26 13:04:18 +1100

    Normal update

---
+++ lab3/P2/lab3_P2_TB.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+ 
+module lab3_P2_TB;
+ 
+	// ------------------ Instantiate module ------------------
+	// We are instantiating the module mux2, naming it instantiate_mux2.
+	// select, input1, input2 are controlled by the testbench, output is 
+	// checked  within the testbench
+ 
+	reg [3:0] count;
+	reg [1:0] S,U,V,W,X;
+	
+	wire [6:0] output_7seg;
+	lab3_P2 instantiate_p5 (.s(S),.u(U),.v(V),.w(W),
+								.x(X),.chosen_7segCode(output_7seg));
+	
+	initial begin
+		$dumpfile("testbench.vcd");
+		$dumpvars(0, lab3_P2_TB);
+		#300;
+		$finish;
+	end
+
+	initial begin 
+		count = 4'b0000;
+	end
+	
+	always begin
+		#50
+		count=count+4'b0001;
+	end
+	
+	always @(count) begin
+		case (count)
+			4'b0000 : begin S = 2'b00; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
+			4'b0001 : begin S = 2'b01; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
+			4'b0010 : begin S = 2'b10; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
+			4'b0011 : begin S = 2'b11; U = 2'b00; V = 2'b01; W = 2'b10; X = 2'b11; end
+			4'b0100 : begin S = 2'b01; U = 2'b00; V = 2'b01; W = 2'b11; X = 2'b11; end
+			default : begin S = 2'b00; U = 2'b00; V = 2'b00; W = 2'b00; X = 2'b00; end
+		endcase
+	end
+
+ 
+endmodule
