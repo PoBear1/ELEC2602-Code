b07e349489075f3467888ad4ae579e0f32ef97bf
po.bear169 <po.bear169@gmail.com>
2026-04-23 13:25:37 +1000

    Updating Lab 5

--- lab5/p1/ctr.v
+++ lab5/p1/ctr.v
@@ -1,16 +1,13 @@
 module fast_ctr(clock, T, rst, q);
 	input clock, T, rst
-	output reg[31:0] q;
+	output wire[31:0] q;
 	reg[31:0]; t
 	assign t[0] <= T;
-	genvar j;
-	for(j = 1; j < 32; j = j + 1) begin
-		assign t[j] = q[j] & t[j - 1];
-	end
 	genvar i;
 	generate
 		for(i = 0; i < 32; i = i + 1) begin
 			pos_t_ff ff(.t(t[i]), .clk(clock), .q(q[i]), .rst(rst));
+			assign t[j] = q[j] & t[j - 1];
 		end
 	endgenerate
 	always @(q) begin
